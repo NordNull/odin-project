b7ecd7c926bedefc7f30a97754c88fd6a9c3f0c0
NordNull <nordnulldev@gmail.com>
2025-06-12 22:33:06 -0400

    starting game logic

---
+++ rock-paper-scissors/scripts.js
@@ -0,0 +1,21 @@
+main(playerChoice) {
+
+    const currentRound = document.getElementById("round-counter-text");
+    const playerChoice = document.getElementById("user-choice");
+    const computerChoice = document.getElementById("computer-choice");
+    const playerScore = document.getElementById("user-score");
+    const computerScore = document.getElementById("computer-score");
+
+    //get user choice
+
+    //get computer choice
+
+    //determine winner
+    
+
+
+    //update score
+
+    //update round
+
+}
